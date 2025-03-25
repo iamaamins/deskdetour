c00c005f5e79306edd2d211ec719845875622a40
Alamin Shaikh <hello@octib.com>
2025-03-25 14:53:22 +0600

    Updated windows icon

--- forge.config.ts
+++ forge.config.ts
@@ -17,7 +17,7 @@ const config: ForgeConfig = {
   },
   rebuildConfig: {},
   makers: [
-    new MakerSquirrel({}),
+    new MakerSquirrel({ iconUrl: '', setupIcon: './src/assets/icon.ico' }),
     new MakerDMG({
       format: 'ULFO',
       background: './src/assets/DMGInstallerBackground.png',
