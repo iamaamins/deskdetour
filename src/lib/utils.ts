2ee51d332e2e0271c9b536dba42aff520be1c195
Alamin Shaikh <hello@octib.com>
2025-03-26 11:10:46 +0600

    Testing wav files for notification sound

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -24,7 +24,7 @@ export async function setApplicationAsLoginItem(app: App) {
 }
 
 export function playNotificationSound(app: App, type: 'view' | 'move') {
-  const filename = `${type}-notification.mp3`;
+  const filename = `${type}-notification.wav`;
   const filePath = isDev
     ? path.join(app.getAppPath(), 'src', 'assets', filename)
     : path.join(process.resourcesPath, 'assets', filename);
@@ -33,6 +33,6 @@ export function playNotificationSound(app: App, type: 'view' | 'move') {
 
   if (isWin)
     return exec(
-      `powershell -c (New-Object WMPLib.WindowsMediaPlayer).controls.play(); (New-Object -ComObject WMPLib.WindowsMediaPlayer).url = '${filePath}';`,
+      `powershell -c (New-Object Media.SoundPlayer '${filePath}').PlaySync();`,
     );
 }
