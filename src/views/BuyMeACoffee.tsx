ea26ab872e376cabb321640f196fe0b9a7fa4db6
Alamin Shaikh <hello@octib.com>
2025-04-23 10:58:49 +0600

    Updated buy me a coffee setup

--- src/views/BuyMeACoffee.tsx
+++ src/views/BuyMeACoffee.tsx
@@ -1,9 +1,10 @@
-import buyMeACoffeeQR from '../assets/buy-me-a-coffee-qr.jpg';
+import bmcButton from '../assets/bmc-button.png';
+import { Link } from 'react-router';
 
 export default function BuyMeACoffee() {
   return (
     <main className='mx-auto w-xl'>
-      <section className='flex h-screen flex-col items-center justify-center space-y-4 text-center'>
+      <section className='flex h-screen flex-col items-center justify-center space-y-6 text-center'>
         <div className='max-w-md space-y-2'>
           <h1 className='text-3xl font-bold'>Buy Me a Coffee</h1>
           <p className='opacity-90'>
@@ -11,11 +12,13 @@ export default function BuyMeACoffee() {
             for everyone!
           </p>
         </div>
-        <img
-          src={buyMeACoffeeQR}
-          alt='Buy me a coffee QR code'
-          className='h-72 w-72 rounded-4xl'
-        />
+        <Link to='https://www.buymeacoffee.com/iamaamins' target='_blank'>
+          <img
+            src={bmcButton}
+            alt='Buy Me A Coffee'
+            className='h-[60px] w-[217px]'
+          />
+        </Link>
         <p className='text-sm opacity-90'>Thank you for your support!</p>
       </section>
     </main>
