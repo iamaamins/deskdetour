b2f5a686607284f593cc7ccccc8be6187f70da4b
Alamin Shaikh <hello@octib.com>
2025-04-23 13:06:08 +0600

    Updated BMC screen

--- src/views/BuyMeACoffee.tsx
+++ src/views/BuyMeACoffee.tsx
@@ -13,24 +13,27 @@ export default function BuyMeACoffee() {
             Detour free for everyone!
           </p>
         </div>
-
         <div className='flex flex-col items-center space-y-2'>
-          <img src={bmcQr} className='h-56 w-56 rounded-md' />
+          <img
+            src={bmcQr}
+            alt='Buy me a coffee QR code'
+            className='h-56 w-56 rounded-md'
+          />
           <p className='text-sm opacity-90'>Scan this QR code</p>
           <div className='flex items-center gap-2'>
-            <div className='bg-slight-gray h-0.5 w-24'></div>
+            <div className='bg-slight-gray h-[1px] w-24'></div>
             <span className='text-sm opacity-90'>OR</span>
-            <div className='bg-slight-gray h-0.5 w-24'></div>
+            <div className='bg-slight-gray h-[1px] w-24'></div>
           </div>
           <Link to='https://www.buymeacoffee.com/iamaamins' target='_blank'>
             <img
               src={bmcButton}
-              alt='Buy Me A Coffee'
-              className='h-auto w-56'
+              alt='Buy me a coffee button'
+              className='h-auto w-56 rounded-md'
             />
           </Link>
         </div>
-        <p className='text-sm opacity-90'>Thank you for your support 💛</p>
+        <p className='text-sm opacity-90'>Thanks for your support 💛</p>
       </section>
     </main>
   );
