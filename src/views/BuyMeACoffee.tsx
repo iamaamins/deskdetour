9005875b38ecc6cd98c15cc2da9a7e72835fa403
Alamin Shaikh <hello@octib.com>
2025-04-23 12:52:37 +0600

    Updated BMC screen

--- src/views/BuyMeACoffee.tsx
+++ src/views/BuyMeACoffee.tsx
@@ -1,4 +1,5 @@
 import bmcButton from '../assets/bmc-button.png';
+import bmcQr from '../assets/bmc-qr.png';
 import { Link } from 'react-router';
 
 export default function BuyMeACoffee() {
@@ -12,13 +13,23 @@ export default function BuyMeACoffee() {
             Detour free for everyone!
           </p>
         </div>
-        <Link to='https://www.buymeacoffee.com/iamaamins' target='_blank'>
-          <img
-            src={bmcButton}
-            alt='Buy Me A Coffee'
-            className='h-[60px] w-[217px]'
-          />
-        </Link>
+
+        <div className='flex flex-col items-center space-y-2'>
+          <img src={bmcQr} className='h-56 w-56 rounded-md' />
+          <p className='text-sm opacity-90'>Scan this QR code</p>
+          <div className='flex items-center gap-2'>
+            <div className='bg-slight-gray h-0.5 w-24'></div>
+            <span className='text-sm opacity-90'>OR</span>
+            <div className='bg-slight-gray h-0.5 w-24'></div>
+          </div>
+          <Link to='https://www.buymeacoffee.com/iamaamins' target='_blank'>
+            <img
+              src={bmcButton}
+              alt='Buy Me A Coffee'
+              className='h-auto w-56'
+            />
+          </Link>
+        </div>
         <p className='text-sm opacity-90'>Thank you for your support 💛</p>
       </section>
     </main>
